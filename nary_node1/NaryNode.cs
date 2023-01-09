baa2061f30b84335ecddce0880534271f098d3de
Adam Gutierrez <adam.gutierrez@hyland.com>
2023-01-09 14:50:09 -0500

    Add project files.

---
+++ nary_node1/NaryNode.cs
@@ -0,0 +1,29 @@
+﻿namespace nary_node1
+{
+	public class NaryNode<T>
+	{
+		T Value;
+		List<NaryNode<T>> Children;
+
+		public NaryNode(T value)
+		{
+			Value = value;
+			Children = new List<NaryNode<T>>();
+		}
+
+		public void AddChild(NaryNode<T> child)
+		{
+			Children.Add(child);
+		}
+
+		public override string ToString()
+		{
+			string output = $"{Value}:";
+			foreach (NaryNode<T> child in Children) 
+			{
+				output += $" {child.Value}";
+			}
+			return output;
+		}
+	}
+}
---
+++ nary_node1/Program.cs
@@ -0,0 +1,37 @@
+﻿using nary_node1;
+
+NaryNode<string> rootNode = new NaryNode<string>("Root");
+NaryNode<string> aNode = new NaryNode<string>("A");
+NaryNode<string> bNode = new NaryNode<string>("B");
+NaryNode<string> cNode = new NaryNode<string>("C");
+NaryNode<string> dNode = new NaryNode<string>("D");
+NaryNode<string> eNode = new NaryNode<string>("E");
+NaryNode<string> fNode = new NaryNode<string>("F");
+NaryNode<string> gNode = new NaryNode<string>("G");
+NaryNode<string> hNode = new NaryNode<string>("H");
+NaryNode<string> iNode = new NaryNode<string>("I");
+
+rootNode.AddChild(aNode);
+rootNode.AddChild(bNode);
+rootNode.AddChild(cNode);
+
+aNode.AddChild(dNode);
+aNode.AddChild(eNode);
+
+cNode.AddChild(fNode);
+
+dNode.AddChild(gNode);
+
+fNode.AddChild(hNode);
+fNode.AddChild(iNode);
+
+Console.WriteLine(rootNode.ToString());
+Console.WriteLine(aNode.ToString());
+Console.WriteLine(bNode.ToString());
+Console.WriteLine(cNode.ToString());
+Console.WriteLine(dNode.ToString());
+Console.WriteLine(eNode.ToString());
+Console.WriteLine(fNode.ToString());
+Console.WriteLine(gNode.ToString());
+Console.WriteLine(hNode.ToString());
+Console.WriteLine(iNode.ToString());
